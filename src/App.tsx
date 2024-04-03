44d4296a2ba48762df5f0496390812c52b6d1658
Jasnoor Saini <jasnoor.saini@icloud.com>
2024-04-02 20:29:43 -0400

    Trying to add me name

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+          Jasnoor Saini
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
