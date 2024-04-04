57b16f6543f16a04c61b867f940a7147f6c4d2d7
Greg Turbe <greturbe@gmail.com>
2024-04-04 16:40:54 -0400

    added name

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload. Names: 
           Carter McCabe, 
           Nathanael Pierre-Louis, 
-          Greg Turbe
+          Gregory Turbe
         </p>
         <a
           className="App-link"
