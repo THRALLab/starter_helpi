fd0d540d80bdd38b53bb8cce1228fb07452945b5
Barry Peirson <barryp@udel.edu>
2024-04-04 15:33:36 -0400

    Created a simple home button that just logs when clicked for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
+import { DetailedQuestion } from './components/DetailedQuestion';
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
@@ -23,17 +24,11 @@ function App() {
   function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
     setKey(event.target.value);
   }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          The Goats
-        </h1>
-        <br></br>
-        <br></br>
-        <h3>
-          Group Members: Isaac Weber, Dylan Minchhoff, Barry Peirson
-        </h3>
+        <Button onClick={ () => console.log("home :)") }>Home</Button>
       </header>
       <Form>
         <Form.Label>API Key:</Form.Label>
