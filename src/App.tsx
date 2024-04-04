415c1c6a8021e568d871e3abb0dedfbca6fe866e
pgsweet <pgsweet@udel.edu>
2024-04-04 15:55:01 -0400

    test to see if the website loads

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
 
-// this is a test
-//initialized branch, cloned repo, doing pull request to get live web page
+// this is a test to check if the website deploys correctly
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
