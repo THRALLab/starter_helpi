c5af4951e179a76b567837d4aa80881bdb7ebbca
Qingjian <xqj@udel.edu>
2024-04-04 04:39:25 -0700

    Add my name to the site

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
           <div>Zhicheng Liu</div>
+          <div>Qingjian Xu</div>
         </p>
         <a
           className="App-link"
