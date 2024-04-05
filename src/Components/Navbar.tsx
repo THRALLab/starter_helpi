6a1c227f56aa662783b83a68fb34f6f6e712da1b
Colin Parsons <colinparsons5@gmail.com>
2024-04-05 13:14:40 -0400

    Work on adjusting how the new logo fits on the Navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,12 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../Components/SmallerWW-removebg-preview.png'
 
 function Navbar2() {
   return (
-    <Navbar fixed="top" bg="*" data-bs-theme="dark" >
+    <Navbar fixed="top" bg="*" data-bs-theme="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">Job Compatability Test</Navbar.Brand>
+          <Navbar.Brand href="#home" className='brand-logo'>
+            <img 
+                src={logo}
+                className="logo-img"
+            />
+          </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="https://michaellutz1.github.io/starter_helpi/">Home</Nav.Link>
             <Nav.Link href="#Basic">Basic Quiz</Nav.Link>
