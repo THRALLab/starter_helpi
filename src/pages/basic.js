758ca947b926e36c02b2d62aeb418edbb5b6d846
pgsweet <pgsweet@udel.edu>
2024-04-05 13:10:17 -0400

    added all webpage files for testing

---
+++ src/pages/basic.js
@@ -0,0 +1,11 @@
+import React from "react";
+ 
+const Basic = () => {
+    return (
+        <div>
+            <h1>Basic Questions page</h1>
+        </div>
+    );
+};
+ 
+export default Basic;
---
+++ src/pages/detailed.js
@@ -0,0 +1,11 @@
+import React from "react";
+ 
+const Detailed = () => {
+    return (
+        <div>
+            <h1>Detailed Questions Page</h1>
+        </div>
+    );
+};
+ 
+export default Detailed;
