6fe7088da2b080e0a422ef36a488f0f7e3b4fe3e
IsaacWeber1 <isaacgweber@gmail.com>
2024-04-06 13:57:52 -0400

    working on data structure

--- src/Components/MC_MULTI_RESPONSE.tsx
+++ src/Components/MC_MULTI_RESPONSE.tsx
@@ -1,3 +1,39 @@
-export function MC_MULTI_RESPONSE(): JSX.Element {
-    return(<></>)
+import { Form, } from 'react-bootstrap';
+export function MC_MULTI_RESPONSE({
+    question,
+    choices,
+    answers,
+    setAnswers
+} : {
+    question: string;
+    choices: string[];
+    answers: string[];
+    setAnswers: (answer: string[]) => void;
+}): JSX.Element {
+    function addAnswer(answer: string) {
+        const found = answers.filter((target: string): boolean => target===answer)
+        if (!found) {
+            setAnswers([...answers, answer])
+        } else {
+            setAnswers(answers.filter((target: string): boolean => target!==answer))
+        }
+    }
+    return (
+        <div>
+            <h3>{question}</h3>
+            <ul style={{ listStyleType: "none" }}>
+                {choices.map((choice) => (
+                    <li>
+                        <Form.Check
+                            type="radio"
+                            id={choice}
+                            label={choice}
+                            onChange={() => addAnswer(choice)}
+                        />
+                    </li>
+                )
+                )}
+            </ul>
+        </div>
+    )
 }
