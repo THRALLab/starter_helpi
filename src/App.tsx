d4379ce25fce77b17faa45ebbdf4f55484735276
Barry Peirson <barryp@udel.edu>
2024-04-08 10:59:42 -0400

    Finishing touches on mockup

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { BasicQuiz } from './pages/BasicQuiz';
+import { AdvancedQuiz } from './pages/AdvancedQuiz'
 // Import any other components or pages you have
 import './App.css';
 
@@ -11,12 +12,13 @@ function App(): JSX.Element {
       <div className="App">
         <header className="App-header">
           <nav>
-            <Link to="/">Home</Link> | <Link to="/basic-quiz">Basic Quiz</Link>
+            <Link to="/">Home</Link> | <Link to="/basic-quiz">Basic Quiz</Link> | <Link to="/advanced-quiz">Advanced Quiz</Link>
           </nav>
         </header>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/basic-quiz" element={<BasicQuiz />} />
+          <Route path="/advanced-quiz" element={<AdvancedQuiz />} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
       </div>
