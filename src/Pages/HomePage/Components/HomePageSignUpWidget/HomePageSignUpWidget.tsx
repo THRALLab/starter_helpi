8f43d27a0157c99e9d5b7d8557bafa5acbbacefe
Jamie <jamie.pacheco.71@gmail.com>
2024-04-08 15:49:41 -0400

    Added new universal styling for buttons. Added sign up widget with text and button Changed header from fixed to sticky for better page layout

--- src/Pages/HomePage/Components/HomePageSignUpWidget/HomePageSignUpWidget.tsx
+++ src/Pages/HomePage/Components/HomePageSignUpWidget/HomePageSignUpWidget.tsx
@@ -3,8 +3,17 @@ import "./HomePageSignUpWidget.css"
 export function HomePageSignUpWidget() : JSX.Element {
 
     return (
-        <div className = "HomePageSignUpWidget">
-            <div className = ""></div>
+        <div className = "home-page-sign-up-widget">
+            <div className = "home-page-sign-up-widget--content">
+                <h1> Before We Get Started...</h1>
+                <div className = "sign-up-widget--text-content">
+                    <p> Consider creating an account! </p>
+                    <p> It helps us get to know you better
+                        allowing us to make a more personalized experience for you.</p>
+                    <p> Plus it is completely free!</p>
+                </div>
+                <button className = "sign-up-button"> Make An Account </button>
+            </div>
         </div>
     )
 }
