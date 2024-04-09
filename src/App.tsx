dd1efec0becd4afaa5e61d7f7621586a2d1e0c22
Jasnoor Saini <jasnoor.saini@icloud.com>
2024-04-09 12:08:44 -0400

    progress on detailed page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 import './App.css';
-import { Button, Form } from 'react-bootstrap';
+//import { Button, Form } from 'react-bootstrap';
 import Navbar from "./components/Navbar";
 import {
     BrowserRouter as Router,
--- src/pages/detailedPage.tsx
+++ src/pages/detailedPage.tsx
@@ -2,14 +2,149 @@ import React from "react";
 
 const detailedPage = () => {
 	return (
-		<div>
-			<h1>
-				Detailed Page Stuff
-			</h1>
-			<>
-			Welcome! For this quiz, you will answer the statements with responses on a range of strongly agree to strongly disagree. This will give us a more in-depth insight into what your career's future could be. Once all statmeents have a response, you will be able to click the "Answer" which will allow you to see the results of you future career.
-			</>
-		</div>
+		<>
+			<div>
+				<h1>
+					Detailed Page Stuff
+				</h1>
+				<>
+					Welcome! For this quiz, you will answer the statements with responses on a range of strongly agree to strongly disagree. This will give us a more in-depth insight into what your career's future could be. Once all statmeents have a response, you will be able to click the "Answer" which will allow you to see the results of you future career.
+				</>
+			</div>
+			<div className="question-box">
+				<h3 className="question-prompt">
+					Question 1: Favorite Disney Princess Movie?
+				</h3>
+				<div className="multiple-answers">
+					<div className="multiple-option">
+						1. Tangled
+					</div>
+					<div className="multiple-option">
+						2. Frozen
+					</div>
+					<div className="multiple-option">
+						3. Cinderella
+					</div>
+					<div className="multiple-option">
+						4. Moana
+					</div>
+				</div>
+			</div>
+			<div className="question-box">
+				<h3 className="question-prompt">
+					Question 1: Favorite Disney Princess Movie?
+				</h3>
+				<div className="multiple-answers">
+					<div className="multiple-option">
+						1. Tangled
+					</div>
+					<div className="multiple-option">
+						2. Frozen
+					</div>
+					<div className="multiple-option">
+						3. Cinderella
+					</div>
+					<div className="multiple-option">
+						4. Moana
+					</div>
+				</div>
+			</div>
+			<div className="question-box">
+				<h3 className="question-prompt">
+					Question 1: Favorite Disney Princess Movie?
+				</h3>
+				<div className="multiple-answers">
+					<div className="multiple-option">
+						1. Tangled
+					</div>
+					<div className="multiple-option">
+						2. Frozen
+					</div>
+					<div className="multiple-option">
+						3. Cinderella
+					</div>
+					<div className="multiple-option">
+						4. Moana
+					</div>
+				</div>
+			</div>
+			<div className="question-box">
+				<h3 className="question-prompt">
+					Question 1: Favorite Disney Princess Movie?
+				</h3>
+				<div className="multiple-answers">
+					<div className="multiple-option">
+						1. Tangled
+					</div>
+					<div className="multiple-option">
+						2. Frozen
+					</div>
+					<div className="multiple-option">
+						3. Cinderella
+					</div>
+					<div className="multiple-option">
+						4. Moana
+					</div>
+				</div>
+			</div>
+			<div className="question-box">
+				<h3 className="question-prompt">
+					Question 1: Favorite Disney Princess Movie?
+				</h3>
+				<div className="multiple-answers">
+					<div className="multiple-option">
+						1. Tangled
+					</div>
+					<div className="multiple-option">
+						2. Frozen
+					</div>
+					<div className="multiple-option">
+						3. Cinderella
+					</div>
+					<div className="multiple-option">
+						4. Moana
+					</div>
+				</div>
+			</div>
+			<div className="question-box">
+				<h3 className="question-prompt">
+					Question 1: Favorite Disney Princess Movie?
+				</h3>
+				<div className="multiple-answers">
+					<div className="multiple-option">
+						1. Tangled
+					</div>
+					<div className="multiple-option">
+						2. Frozen
+					</div>
+					<div className="multiple-option">
+						3. Cinderella
+					</div>
+					<div className="multiple-option">
+						4. Moana
+					</div>
+				</div>
+			</div>
+			<div className="question-box">
+				<h3 className="question-prompt">
+					Question 1: Favorite Disney Princess Movie?
+				</h3>
+				<div className="multiple-answers">
+					<div className="multiple-option">
+						1. Tangled
+					</div>
+					<div className="multiple-option">
+						2. Frozen
+					</div>
+					<div className="multiple-option">
+						3. Cinderella
+					</div>
+					<div className="multiple-option">
+						4. Moana
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
