72b1fb2166a5583f28d7a0a7e687911ea5b6f8ce
mforte5115 <mforte@udel.edu>
2024-04-08 21:50:52 -0400

    Added 2 cards for the homescreen

--- src/HomeScreen/HomeScreen.tsx
+++ src/HomeScreen/HomeScreen.tsx
@@ -1,29 +1,27 @@
-import logo from './logo.svg';
-
+import './HomeScreen.css'
+import { Card } from 'react-bootstrap';
 
 export function HomeScreen () {
 
 
     return (
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          Michael Lutz
-          Jacob Wilber
-          Colin Parsons
-        </p>
-        <p>
-          Michael Forte
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Left-container"> 
+        <div>
+          <Card className="Home-box">
+            <Card.Body>
+              <Card.Title>Basic Quiz</Card.Title>
+              <Card.Text>A description</Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="Right-container">
+          <Card className="Home-box">
+            <Card.Title>Detailed Quiz</Card.Title>
+            <Card.Text>
+              The description
+            </Card.Text>
+          </Card>
+        </div>
+      </div>
     )
 }
