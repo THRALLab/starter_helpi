c137d0cce7c5cdfb77657029aaf7568e4fa97c30
mforte5115 <mforte@udel.edu>
2024-04-08 22:42:04 -0400

    Added a header but this shit aint working

--- src/HomeScreen/HomeScreen.tsx
+++ src/HomeScreen/HomeScreen.tsx
@@ -5,22 +5,23 @@ export function HomeScreen () {
 
 
     return (
-      <div className="Container"> 
-        <div>
-          <Card className="Home-box">
-            <Card.Body>
-              <Card.Title>Basic Quiz</Card.Title>
-              <Card.Text>A description</Card.Text>
-            </Card.Body>
-          </Card>
+      <div className="Container">
+        <div className="Title">
+          <h1>Select A Quiz:</h1>
         </div>
-        <div className="Right-container">
-          <Card className="Home-box">
-            <Card.Title>Detailed Quiz</Card.Title>
-            <Card.Text>
-              The description
-            </Card.Text>
-          </Card>
+        <div className="Both-boxes">
+            <Card className="Home-box">
+              <Card.Body>
+                <Card.Title>Basic Quiz</Card.Title>
+                <Card.Text>A description</Card.Text>
+              </Card.Body>
+            </Card>
+            <Card className="Home-box Right-Box">
+              <Card.Title>Detailed Quiz</Card.Title>
+              <Card.Text>
+                The description
+              </Card.Text>
+            </Card>
         </div>
       </div>
     )
