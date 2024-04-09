01f220883598456433a18c12af93d971ba07704e
Joey <joeydare@comcast.net>
2024-04-09 16:38:29 -0400

    Added detailed button to home page with containters

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -6,22 +6,31 @@ interface HomeProp {
 }
 
 const Home: React.FC<HomeProp> = ({ handlePage }) => {
-    const style: { column: CSSProperties } = {
+    const style: { container: CSSProperties, column: CSSProperties } = {
+        container: {
+            display: 'flex',
+        },
         column: {
             border: '1px solid #ccc',
-            margin: '10',
+            margin: '10px',
             padding: '20px',
             display: 'flex',
+            flexDirection: 'column',
             alignItems: 'center',
-            flexDirection: 'column'
-        }
+        },
     };
     return (
     <>
-    <div style={style.column}>
-    <h1>Basic Questions</h1>
-        <Button className="Basic-Button" onClick={() => handlePage('Basic')}>Basic Questions</Button>
-    </div>
+    <div style={style.container}>
+            <div style={style.column}>
+                <h1>Basic Questions</h1>
+                <Button className="Basic-Button" onClick={() => handlePage('Basic')}>Basic Questions</Button>
+            </div>
+            <div style={style.column}>
+                <h1>Detailed Questions</h1>
+                <Button className="Detailed-Button" onClick={() => handlePage('Detailed')}>Detailed Questions</Button>
+            </div>
+        </div>
     </>
     );
 }
