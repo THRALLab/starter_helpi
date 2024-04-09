ccbea2c505d88f6384e9ac342ec68ff55c131b63
Jasnoor Saini <jasnoor.saini@icloud.com>
2024-04-09 19:00:38 -0400

    Have 2/3 of the potentail prompts done

--- src/pages/detailedPage.tsx
+++ src/pages/detailedPage.tsx
@@ -9,12 +9,15 @@ const detailedPage = () => {
 					Detailed Page Stuff
 				</h1>
 				<>
-					Welcome! For this quiz, you will answer the statements with responses on a range of strongly agree to strongly disagree. This will give us a more in-depth insight into what your career's future could be. Once all statmeents have a response, you will be able to click the "Answer" which will allow you to see the results of you future career.
+					Welcome! For this quiz, you will answer the statements by 
+					choosing one of the corresponding multiple choice options
+					 below! You will be able to click the "Answer" which will 
+					 allow you to see the results of you future career.
 				</>
 			</div>
 			<div className="question-box">
 				<h3 className="question-prompt">
-					Question 1: Favorite Disney Princess Movie?
+					Question 1: The question can be really long or short. It will all fit in here
 				</h3>
 				<div className="multiple-answers">
 					<div className="answer-option">
@@ -36,23 +39,23 @@ const detailedPage = () => {
 				</div>
 			</div>
 			<div className="question-box">
-				<h3 className="question-prompt">
-					Question 1: Favorite Disney Princess Movie?
+				<h3 style={{display: "inline-block"}} className="question-prompt">
+					Maybe put options in the same line as question?
 				</h3>
-				<div className="multiple-answers">
-					<div className="answer-option">
+				<div style={{display: "inline-block"}} className="multiple-answers">
+					<div style={{display: "inline-block", padding: "18px"}} className="answer-option">
 						<input type="radio" id="movie1" name="movie" value="Tangled" />
 						<label htmlFor="age1">Tangled</label>
 					</div>
-					<div className="answer-option">
+					<div style={{display: "inline-block", padding: "18px"}} className="answer-option">
 						<input type="radio" id="movie2" name="movie" value="Frozen" />
 						<label htmlFor="age1">Frozen</label>
 					</div>
-					<div className="answer-option">
+					<div style={{display: "inline-block", padding: "18px"}} className="answer-option">
 						<input type="radio" id="movie3" name="movie" value="Cinderella" />
 						<label htmlFor="age1">Cinderella</label>
 					</div>
-					<div className="answer-option">
+					<div style={{display: "inline-block", padding: "18px"}} className="answer-option">
 						<input type="radio" id="movie4" name="movie" value="Moana" />
 						<label htmlFor="age1">Moana</label>
 					</div>
@@ -60,9 +63,9 @@ const detailedPage = () => {
 			</div>
 			<div className="question-box">
 				<h3 className="question-prompt">
-					Question 1: Favorite Disney Princess Movie?
+					Stack options on one another rather than side by side?
 				</h3>
-				<div className="multiple-answers">
+				<div style={{display: "block"}} className="multiple-answers">
 					<div className="answer-option">
 						<input type="radio" id="movie1" name="movie" value="Tangled" />
 						<label htmlFor="age1">Tangled</label>
