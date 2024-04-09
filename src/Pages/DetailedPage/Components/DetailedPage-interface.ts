71475e7116ee8a2fa7849d1a3f352fea500e5ea3
water-plant <arsconduit@gmail.com>
2024-04-09 15:14:49 -0400

    navigation interface

--- src/Pages/DetailedPage/Components/DetailedPage-interface.ts
+++ src/Pages/DetailedPage/Components/DetailedPage-interface.ts
@@ -6,6 +6,12 @@ export interface question {
     answer: number;
     body: string;
 }
+
+export interface navigation {
+    max: number;
+    current: number;
+}
+
 export function generatePlaceholders(type: string): question { 
     return {name:"Lorem ipsum dolor sit amet", questionNumber:1, type: type, options:["one", "two", "three"], answer: -1, body:"consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."};
 }
