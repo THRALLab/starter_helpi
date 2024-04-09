766e1766c92091005115a098f6a05030e82c426c
Alex Hoy <aghoy03@gmail.com>
2024-04-09 18:45:44 -0400

    form.range being used in detailed questions

--- src/DetailedQuestions.tsx
+++ src/DetailedQuestions.tsx
@@ -13,6 +13,8 @@ if (prevKey !== null) {
 
 function DetailedQuestions() {
   const [key, setKey] = useState<string>(keyData); //for api key input
+  const [sliderValue1, setSliderValue1] = useState<number>(50);
+  const [sliderValue2, setSliderValue2] = useState<number>(50);
   
   //sets the local storage item to the api key the user inputed
   function handleSubmit() {
@@ -24,31 +26,36 @@ function DetailedQuestions() {
   function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
     setKey(event.target.value);
   }
+
+  function updateSliderValue1 (event: React.ChangeEvent<HTMLInputElement>){
+    setSliderValue1(parseInt(event.target.value));
+  }
+
+  function updateSliderValue2 (event: React.ChangeEvent<HTMLInputElement>){
+    setSliderValue2(parseInt(event.target.value));
+  }
+
   return (
     <div className="DetailedQuestions">
       <header className="General-header"> <p>The Career Lab <LinkButton to="/" label="Home"></LinkButton></p> </header>
       <header className="DetailedQuestions-header">
-        <p>
-          Edit <code>src/DetailedQuestions.tsx</code> and save to reload.
-        </p>
-        <p>
-          Alex Hoy Branch
-        </p>
-        <a
-          className="DetailedQuestions-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label>Question 1:</label><br />
+        <Form.Range style={{ transform: 'rotate(270deg)', width: '90px', marginLeft: '100px' }} value={sliderValue1} onChange={updateSliderValue1}/>
+        <p>Selected Value: {sliderValue1}</p>
+
+        <label>Question 2:</label><br />
+        <Form.Range style={{ transform: 'rotate(270deg)', width: '90px', marginLeft: '100px' }} value={sliderValue2} onChange={updateSliderValue2} />
+        <p>Selected Value: {sliderValue2}</p>
+
       </header>
-      <Form>
-        <Form.Label>API Key:</Form.Label>
-        <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
-        <br></br>
-        <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
-      </Form>
+      <div className='API-Footer'>
+        <Form>
+          <Form.Label>API Key:</Form.Label>
+          <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
+          <br></br>
+          <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
+        </Form>
+        </div>
     </div>
   );
 }
