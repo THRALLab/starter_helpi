b1150b2bb650f94e2c755826eff5cb676f56def5
Coleman <cowalsh@udel.edu>
2024-04-09 12:31:24 -0400

    first draft of stylized nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Nav, NavLink, NavMenu } from "./NavbarElements";
+import './Navbar.css';
  
 const Navbar = () => {
     return (
         <>
             <Nav>
+                <div className="site-title">
+                    <h1>Website Title</h1>
+                </div>
                 <NavMenu>
                     <NavLink to="/">
                         Home
