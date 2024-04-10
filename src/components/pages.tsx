5448c80a881009238b190bad8b0575f20573d4fa
Jessica-C2004 <jacunnin@udel.edu>
2024-04-09 22:53:49 -0400

    added basic question description, still fixing format

--- src/components/pages.tsx
+++ src/components/pages.tsx
@@ -1,8 +1,25 @@
+import { Col, Container, Row } from "react-bootstrap";
 import { AIKey } from "../interfaces/AIKeyInterface";
 
 export function HomePage(key: AIKey): JSX.Element {
-    return <div>
+    return <div className="Pages">
         homePage
+        <Container>
+            <Row>
+                <Col>
+                    <div className="question-description">
+                        The basic questions will ask you more simple, easier<br></br>
+                        questions that will give you a quick idea about your<br></br> 
+                        potential ideal career.
+                    </div>
+                </Col>
+                <Col>
+                    <div className="Question-description">
+                        Detailed questions here
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     </div>;
 }
 
