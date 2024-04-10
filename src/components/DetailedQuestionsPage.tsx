700aac086af8eae3b88a205a496846c1b577326a
rubina <srubina886@gmail.com>
2024-04-10 18:32:07 -0400

    added basic and detailed questions buttons

---
+++ src/components/DetailedQuestionsPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export function DetailedQuestionsPage(): JSX.Element {
+    return (
+        <div>
+            <p>Detailed Questions Page</p>
+        </div>
+    )
+}
