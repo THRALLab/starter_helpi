f060741850294150088bc99db71a19ad9b2da3e3
Jamie <jamie.pacheco.71@gmail.com>
2024-04-10 17:40:04 -0400

    added sign up page and user edited user interface

--- src/Pages/SignUpPage/SignUpPage.tsx
+++ src/Pages/SignUpPage/SignUpPage.tsx
@@ -1,9 +1,23 @@
+import { Form } from "react-bootstrap"
+import "./SignUpPage.css"
 
 export function SignUpPage() : React.JSX.Element {
 
     return (
         <div className = "sign-up-page">
-            
+            <div className = "sign-up-page--content">
+                <div className = "sign-up-page--form-container"> 
+                    <h1>Let's Make An Account! </h1>
+                    <div className = "form-container--content">
+                        <Form>
+                            <Form.Group className="mb-3 col">
+                                <Form.Label> Name </Form.Label>
+                                <Form.Control/>
+                            </Form.Group>
+                        </Form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
