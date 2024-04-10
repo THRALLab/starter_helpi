7cea1490b5e82dd06a48b74686fbefeeaae101ba
Colin Parsons <colinparsons5@gmail.com>
2024-04-10 09:47:07 -0400

    Navbar now functional for switching between pages. It also now displays what page you are currently on and has a primitive back button

--- src/Components/DetailedPage/DetailedPage.tsx
+++ src/Components/DetailedPage/DetailedPage.tsx
@@ -3,7 +3,9 @@ import './DetailedPage.css'
 export function DetailedPage() {
     return (
         <div>
+            <body>
             Detailed Question Page
+            </body>
         </div>
     )
 }
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,22 @@
 import Container from 'react-bootstrap/Container';
+import React, { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from './SmallerWW-removebg-preview.png'
 import '../Navbar/Navbar2.css'
 
 export function Navbar2({page, setPage}: {page:string; setPage: (newPage: string) => void}) {
+  const [previousPage, setPreviousPage] = useState<string>("Home");
+
+  const handleNavClick = (newPage: string) => {
+    setPreviousPage(page);
+    setPage(newPage);
+  }
+
+  const handleBackButtonClick = () => {
+    setPage(previousPage);
+  }
+
   return (
     <Navbar fixed="top" bg="dark bg-dark gradient-custom rounded" style={{height: '4rem', marginTop: '.25rem', marginRight: '.25rem', marginLeft: '.25rem'}} data-bs-theme="dark" expand="lg">
         <Container>
@@ -12,10 +24,16 @@ export function Navbar2({page, setPage}: {page:string; setPage: (newPage: string
             <img src={logo} className="logo-img"/>
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link onClick={() => setPage("Home")}>Home</Nav.Link>
-            <Nav.Link onClick={() => setPage("Basic")}>Basic Quiz</Nav.Link>
-            <Nav.Link onClick={() => setPage("Detailed")}>Detailed Quiz</Nav.Link>
+            <Nav.Link onClick={() => handleNavClick("Home")}>Home</Nav.Link>
+            <Nav.Link onClick={() => handleNavClick("Basic")}>Basic Quiz</Nav.Link>
+            <Nav.Link onClick={() => handleNavClick("Detailed")}>Detailed Quiz</Nav.Link>
           </Nav>
+          {page !== "Home" && (
+            <Nav.Link onClick={handleBackButtonClick}>Back</Nav.Link>
+          )}
+          <span className="navbar-text">
+            Current Page: {page}
+          </span>
         </Container>
       </Navbar>
   );
