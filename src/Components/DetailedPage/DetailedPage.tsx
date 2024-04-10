91fd6bee2b33c067cede0fdf9bd7290402b05635
jacobwilbe <jacobwilbe@users.noreply.github.com>
2024-04-10 18:21:33 -0400

    fixed merge conflicts

--- src/Components/DetailedPage/DetailedPage.tsx
+++ src/Components/DetailedPage/DetailedPage.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import './DetailedPage.css'
 
-let questions: string[] = ["question 1", "question 2", "question 3"];
 export function DetailedPage() {
     return (
-<<<<<<< HEAD
         <div>Detailed Page</div>
-=======
-        <div>
-            Detailed Question Page
-        </div>
->>>>>>> main
     )
 
 }
