cdf291f7810088bd9764eb9a90816e9c9f75d27c
shing rue lin <shingrue@udel.edu>
2024-04-10 15:44:37 -0400

    Added basic description

--- src/components/homePageComp/decriptionTable.tsx
+++ src/components/homePageComp/decriptionTable.tsx
@@ -1,16 +1,24 @@
-
 const DescriptionTable: React.FC = () => {
-    return <div className="container">
-        <div className="section">
-            <h2 className="header">Basic</h2>
-            <p className="paragraph">The basic assessment......................................</p>
+    return (
+        <div className="container">
+            <div className="section">
+                <h2 className="header">Basic</h2>
+                <p className="paragraph">
+                    The basic assessment quiz is a short questionaire designed
+                    to gauge your knowledge and skills with simple and clear
+                    questions. With immediate feedback, you will know which
+                    career is right for you in a matter of a few minutes!{" "}
+                </p>
+            </div>
+            <div className="section">
+                <h2 className="header">Detailed</h2>
+                <p className="paragraph">
+                    The Detailed assessment will consist of more questions and
+                    will give you a more accurate and detailed assesment of what
+                    carreer you should choose.
+                </p>
+            </div>
         </div>
-        <div className="section">
-            <h2 className="header">Detailed</h2>
-            <p className="paragraph">The Detailed assessment will consist of more
-                                     questions and will give you a more 
-                                     accurate and detailed assesment of what carreer you should choose.</p>
-        </div>
-    </div>;
-}
+    );
+};
 export default DescriptionTable;
