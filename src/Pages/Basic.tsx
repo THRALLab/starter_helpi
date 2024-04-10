b333392c4b54cd5cce167e2eec576f3506cb11b3
Joey <joeydare@comcast.net>
2024-04-10 19:06:35 -0400

    Added Pages.css and moved styling into css

--- src/Pages/Basic.tsx
+++ src/Pages/Basic.tsx
@@ -1,5 +1,5 @@
 
-import React, { CSSProperties } from 'react';
+import React, {} from 'react';
 import {Button} from 'react-bootstrap';
 
 interface BasicProp {
@@ -7,17 +7,7 @@ interface BasicProp {
 }
 
 const Basic: React.FC<BasicProp> = ({ handlePage }) => {
-    const style: { column: CSSProperties } = {
-        column: {
-            border: '1px solid #ccc',
-            margin: '10',
-            padding: '20px',
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: 'column'
-        }
-    };
-    return (<div style={style.column}>
+    return (<div className="home-column">
         <h1>Home Page</h1>
         <Button className="Home-Button" onClick={() => handlePage('Home')}>Home Page</Button>
     </div>)
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,38 +1,27 @@
-import React, { CSSProperties } from 'react';
+import React, {} from 'react';
 import {Button} from 'react-bootstrap';
+import './Pages.css';
 
 interface HomeProp {
     handlePage: (page: string) => void;
 }
 
 const Home: React.FC<HomeProp> = ({ handlePage }) => {
-    const style: { container: CSSProperties, column: CSSProperties } = {
-        container: {
-            display: 'flex',
-            justifyContent: 'space-between', 
-        },
-        column: {
-            border: '1px solid #ccc',
-            margin: '10px',
-            padding: '20px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-        },
-    };
     return (
-    <>
-    <div style={style.container}>
-            <div style={style.column}>
+    <div className="home-container">
+            <div className="home-column">
+                <h1>Home Page</h1>
+                <Button className="Home-Button" onClick={() => handlePage('Home')}>Home Page</Button>
+            </div>
+            <div className="home-column">
                 <h1>Basic Questions</h1>
                 <Button className="Basic-Button" onClick={() => handlePage('Basic')}>Basic Questions</Button>
             </div>
-            <div style={style.column}>
+            <div className="home-column">
                 <h1>Detailed Questions</h1>
                 <Button className="Detailed-Button" onClick={() => handlePage('Detailed')}>Detailed Questions</Button>
             </div>
         </div>
-    </>
     );
 }
 
