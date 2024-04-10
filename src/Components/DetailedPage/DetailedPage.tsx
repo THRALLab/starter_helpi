f4797a66d3ea3ffbe9920591141454c19ed720c6
jacobwilbe <jacobwilbe@users.noreply.github.com>
2024-04-10 18:13:37 -0400

    added accordions

--- src/Components/DetailedPage/DetailedPage.tsx
+++ src/Components/DetailedPage/DetailedPage.tsx
@@ -1,11 +1,10 @@
+import React from 'react'
 import './DetailedPage.css'
 
+let questions: string[] = ["question 1", "question 2", "question 3"];
 export function DetailedPage() {
     return (
-        <div>
-            <body>
-            Detailed Question Page
-            </body>
-        </div>
+        <div>Detailed Page</div>
     )
+
 }
