0efd6a213d01eae5e283d9044046833fbe748a3c
pgsweet <pgsweet@udel.edu>
2024-04-11 13:28:52 -0400

    changed colors and added rounded edges to buttons and boxes

--- src/components/pages.tsx
+++ src/components/pages.tsx
@@ -5,17 +5,17 @@ import { AIKey } from "../interfaces/AIKeyInterface";
 export function HomePage(key: AIKey): JSX.Element {
     return <div className="Pages">
         <h3 className="Page-title">Home Page</h3>
-            <Container>
+            <Container className="Default-box">
                 <center>
                     <h4 className="Description-header">Basic Questions</h4>
-                    <div className="basic-description">
+                    <div className="Description">
                         The basic questions will ask you more simple, easier
                         questions that will give you a quick idea about your
                         potential ideal career path. For a brief recommendation for 
                         careers, take this quiz!
                     </div>
                     <h4 className="Description-header">Detailed Questions</h4>
-                        <div className="detailed-description">
+                        <div className="Description">
                         The detailed questions quiz is targeted towards narrowing down your optimal field of study. If you would like recommendations for specific careers within your field of interest, take this quiz!  
                         </div>
                 </center>
