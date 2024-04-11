c9048c8a7f9dd9ae98215d99962cbd153ff97692
DylanMinchhoff <dylanminchhoff@gmail.com>
2024-04-11 00:10:11 -0400

    progress-bar and tsConfig quiality of life

--- src/pages/basic-quiz/BasicQuiz.tsx
+++ src/pages/basic-quiz/BasicQuiz.tsx
@@ -1,3 +1,26 @@
+import { useState } from "react"
+import { Row, Container } from "react-bootstrap"
+import { basicQuiz } from "src/assets/Quizzes/BasicQuiz"
+import { DisplayQuiz } from "src/components/DisplayQuiz"
+import { ProgressBar } from "src/components/ProgressBar"
+
+
+
 export const BasicQuiz = () => {
-    return(<>hello</>)
+    const [questionsAnswered, setQuestionsAnswered] = useState<number>(0);
+    const totalQuestions = Object.keys(basicQuiz).length;
+    return(
+    <div className="basicQuiz-container">
+        <Container>
+            <Row>
+                <ProgressBar
+                    value={questionsAnswered}
+                    max={totalQuestions}
+                />
+            </Row>
+            <Row>
+                <DisplayQuiz quiz={basicQuiz}/>
+            </Row>
+        </Container>
+    </div>)
 }
