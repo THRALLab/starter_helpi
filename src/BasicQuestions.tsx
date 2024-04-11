5c1dd9bb9a203cacbe4a568e9850f999ccecbad6
Greg Turbe <greturbe@gmail.com>
2024-04-11 13:58:05 -0400

    added home button to header, centered answers

--- src/BasicQuestions.tsx
+++ src/BasicQuestions.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+
 
 const OPTIONS = [
   "Disagree",
@@ -11,7 +13,7 @@ const OPTIONS = [
 const QUESTIONS = [1,2,3,4,5,6,7,8]
 const BasicQuestions: React.FC = () => {
   const [chosenOption, setChosenOption] = useState<string[]>(Array(QUESTIONS.length).fill(OPTIONS[2]));
-
+  const navigate = useNavigate();
   function updateOption(event: React.ChangeEvent<HTMLInputElement>, index: number) {
     const newOptions = [...chosenOption];
     newOptions[index] = event.target.value;
@@ -19,16 +21,26 @@ const BasicQuestions: React.FC = () => {
   }
   return (
       <div>
-        <h2>Basic Career Aptitude Test </h2>
+        <h2>
+          Basic Career Aptitude Test
+          <nav>
+            <ul>
+              <li>
+                <Button variant="link" onClick={() => navigate('/starter_helpi')}>Return to Home Page</Button>
+              </li>
+            </ul>
+          </nav>
+          
+        </h2>
         <p>
           Answer the following questions to the best of your ability. Then we will 
           then we will use your answers to determine possible career paths for you.
         </p>
         <div>
           {QUESTIONS.map((question, index) => (
-            <div key={question}>
+            <div key={question} style={{ marginBottom: "20px" }}>
               <div>Question {question}</div>
-              <div style={{ display: "flex" }}>
+              <div style={{ display: "flex", justifyContent: "center" }}>
               {OPTIONS.join(', ').split(', ').map((option) => (
                 <Form.Check
                     key={ option }
