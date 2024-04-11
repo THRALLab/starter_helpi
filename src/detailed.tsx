318a53cce5ef2e16fe7fea2d0c1870369e286095
Hairum Qureshi <hqureshi@udel.edu>
2024-04-11 15:12:02 -0400

    resolved redundancy img tag error

--- src/detailed.tsx
+++ src/detailed.tsx
@@ -96,7 +96,7 @@ function Detailed() {
 							<div className="child">
 								<img
 									src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
-									alt="Question image"
+									alt="Question"
 								/>
 							</div>
 							<div className="child">
