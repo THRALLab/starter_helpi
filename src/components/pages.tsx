4d633ef7f82aeff9dd4d036db213a9b4d24567e1
Jessica-C2004 <jacunnin@udel.edu>
2024-04-10 21:02:18 -0400

    updated home page to look more organized

--- src/components/pages.tsx
+++ src/components/pages.tsx
@@ -1,25 +1,25 @@
 import "./pages.css"
-import { Col, Container, Row } from "react-bootstrap";
+import {Container, Row } from "react-bootstrap";
 import { AIKey } from "../interfaces/AIKeyInterface";
 
 export function HomePage(key: AIKey): JSX.Element {
     return <div className="Pages">
-        Home Page
-        <Container>
-            <Row>
-                <header className="Basic-question-header">Basic Questions</header>
-                <div className="basic-description">
-                    The basic questions will ask you more simple, easier
-                    questions that will give you a quick idea about your
-                    potential ideal career path. For a brief recommendation for 
-                    careers, take this quiz!
-                </div>
-                <header className="Detailed-question-header">Detailed Questions</header>
-                    <div className="detailed-description">
-                    The detailed questions quiz is targeted towards narrowing down your optimal field of study. If you would like recommendations for specific careers within your field of interest, take this quiz!  
+        <h3 className="Page-title">Home Page</h3>
+            <Container>
+                <center>
+                    <h4 className="Description-header">Basic Questions</h4>
+                    <div className="basic-description">
+                        The basic questions will ask you more simple, easier
+                        questions that will give you a quick idea about your
+                        potential ideal career path. For a brief recommendation for 
+                        careers, take this quiz!
                     </div>
-            </Row>
-        </Container>
+                    <h4 className="Description-header">Detailed Questions</h4>
+                        <div className="detailed-description">
+                        The detailed questions quiz is targeted towards narrowing down your optimal field of study. If you would like recommendations for specific careers within your field of interest, take this quiz!  
+                        </div>
+                </center>
+            </Container>
     </div>;
 }
 
