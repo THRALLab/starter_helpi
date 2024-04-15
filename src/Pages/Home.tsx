af9b292efb2e9516f3c53ae7f8f6e7010d499f07
Joey <joeydare@comcast.net>
2024-04-15 16:53:23 -0400

    Added brain logo to header

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,17 +1,22 @@
 import React, {} from 'react';
 import {Button} from 'react-bootstrap';
+import BrainIcon from './modifiedBrainIcon.svg';
 import './Pages.css';
 
 interface HomeProp {
     handlePage: (page: string) => void;
 }
-
 const Home: React.FC<HomeProp> = ({ handlePage }) => {
     return (
-        <header className="header">
+    <header className="header">
+        <div className="icon-container">
+        <img src={BrainIcon} alt="Brain Icon" className="icon" />
+        </div>
                 <Button className="home-button" onClick={() => handlePage('Home')}>Home Page</Button>
-                <Button className="basic-button" onClick={() => handlePage('Basic')}>Basic Questions</Button>
-                <Button className="detailed-button" onClick={() => handlePage('Detailed')}>Detailed Questions</Button>
+                <Button className="basic-button" onClick={() => handlePage('Basic')}>Basic Questions
+                </Button>
+                <Button className="detailed-button" onClick={() => handlePage('Detailed')}>Detailed Questions
+                </Button>
         </header>
     );
 }
