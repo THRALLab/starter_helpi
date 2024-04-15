df6a7bf17c5b9ac2540fb9b4db4621b708b70548
Joey <joeydare@comcast.net>
2024-04-15 13:25:12 -0400

    Added hover and border to nav buttons

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -9,17 +9,9 @@ interface HomeProp {
 const Home: React.FC<HomeProp> = ({ handlePage }) => {
     return (
         <header className="header">
-    <div className="home-container">
-            <div className="home-column">
                 <Button className="home-button" onClick={() => handlePage('Home')}>Home Page</Button>
-            </div>
-            <div className="home-column">
                 <Button className="basic-button" onClick={() => handlePage('Basic')}>Basic Questions</Button>
-            </div>
-            <div className="home-column">
                 <Button className="detailed-button" onClick={() => handlePage('Detailed')}>Detailed Questions</Button>
-            </div>
-        </div>
         </header>
     );
 }
