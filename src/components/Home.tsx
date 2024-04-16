996826d38a5bcfc83dc70daf7c95e36e1dcc1abe
rubina <srubina886@gmail.com>
2024-04-16 16:24:22 -0400

    visuals

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import { Buttons } from './Buttons';
 import '/Users/rubinashaik/starter_helpi/src/App.css';
+import Grid from '@mui/material/Grid'; // Grid version 1
+import Item from '@mui/material/ListItem';
+import '/Users/rubinashaik/starter_helpi/src/App.css'
 
 export function Home(): JSX.Element {
     return (
         <div>
-            {/*<header className='App-header'></header>*/}
-            <body className='Home-body'>
-                
-            </body>
-            <Buttons></Buttons>
-        </div>
+        <Buttons></Buttons>
+        
+        <header>
+          <h1>Welcome to My React Web Page!</h1>
+        </header>
+        <main>
+        <div className="page">
+            <div className="container">
+                <div className="box">
+                 <button>Button 1</button>
+            </div>
+             <div className="box">
+                <button>Button 2</button>
+             </div>
+           </div>
+      </div>
+        </main>
+        <footer>
+          <p>Footer content goes here.</p>
+        </footer>
+      </div>
     )
 }
