f48d4ddfe57c041547601ec007c7b868c2fd6fef
Hairum Qureshi <hqureshi@udel.edu>
2024-04-17 17:49:07 -0400

    removed unused variables in App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import React, { useState } from "react";
-// import logo from "./logo.svg";
 import "./App.css";
-import { Button, Form } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Basic from "./basic";
 import Detailed from "./detailed";
