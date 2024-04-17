f6d4a39d1d9060656ec640e12a1f72631a7f683d
Joey <joeydare@comcast.net>
2024-04-17 00:07:27 -0400

    Updated home page significantly to look much more appealing

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,6 +1,8 @@
 import React, {} from 'react';
 import {Button} from 'react-bootstrap';
 import BrainIcon from './modifiedBrainIcon.svg';
+import homeIcon from './house.svg';
+import arrowIcon from './arrow.svg';
 import './Pages.css';
 
 interface HomeProp {
@@ -8,6 +10,7 @@ interface HomeProp {
 }
 const Home: React.FC<HomeProp> = ({ handlePage }) => {
     return (
+        <div>
     <header className="header">
         <div className="hover-container">
         <div className="icon-container">
@@ -16,11 +19,16 @@ const Home: React.FC<HomeProp> = ({ handlePage }) => {
         </div>
         <h2 className="title" onClick ={() => handlePage('Home')}>Brain Spark</h2>
         <div className="home-container">
-                <Button className="home-button" onClick={() => handlePage('Home')}>Home Page</Button>
-                <Button className="basic-button" onClick={() => handlePage('Basic')}>Basic Questions</Button>
-                <Button className="detailed-button" onClick={() => handlePage('Detailed')}>Detailed Questions</Button>
+                <Button className="home-button" onClick={() => handlePage('Home')}><img src={homeIcon} alt="Home Page" className="button-icon" /></Button>
         </div>
         </header>
+        <div className="home-center">
+            <h1 className="center-title">Start your future here</h1>
+            <p className="center-left">Student-made career test<br></br>powered through the use of AI</p>
+            <Button className="basic-button" onClick={() => handlePage('Basic')}>Basic Questions<img src={arrowIcon} alt="arrow" className="arrow-icon" /></Button>
+                <Button className="detailed-button" onClick={() => handlePage('Detailed')}>Detailed Questions<img src={arrowIcon} alt="arrow" className="arrow-icon" /></Button>
+        </div>
+        </div>
     );
 }
 
