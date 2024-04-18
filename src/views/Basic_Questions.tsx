f8db499b65f414b1c15b9c26cebf2e2776f38cdf
Max De Jesus <me@maxdejesus.com>
2024-04-18 14:55:53 -0400

    imports fix

--- src/views/Basic_Questions.tsx
+++ src/views/Basic_Questions.tsx
@@ -1,50 +1,30 @@
-import React, { useState } from 'react';
-import '../App.css';
-import {Button, Form} from "react-bootstrap";
-import {Q1} from "../BasicQuestions/Q1";
-import {Q2} from "../BasicQuestions/Q2";
-import {Q3} from "../BasicQuestions/Q3";
-import {Q4} from "../BasicQuestions/Q4";
-import {Q5} from "../BasicQuestions/Q5";
-import {Q6} from "../BasicQuestions/Q6";
-import {Q7} from "../BasicQuestions/Q7";
-import {Q8} from "../BasicQuestions/Q8";
+import "../App.css";
+import { Q1 } from "../BasicQuestions/Q1";
+import { Q2 } from "../BasicQuestions/Q2";
+import { Q3 } from "../BasicQuestions/Q3";
+import { Q4 } from "../BasicQuestions/Q4";
+import { Q5 } from "../BasicQuestions/Q5";
+import { Q6 } from "../BasicQuestions/Q6";
+import { Q7 } from "../BasicQuestions/Q7";
+import { Q8 } from "../BasicQuestions/Q8";
 import { Footer } from "../components/Footer";
 
 function Basic_Questions(): JSX.Element {
-
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const questions = [<Q1 />, <Q2 />, <Q3 />, <Q4 />, <Q5 />, <Q6 />, <Q7 />, <Q8 />];
-
-  const handleNextClick = () => {
-    if (currentQuestion < questions.length - 1) {
-      setCurrentQuestion(currentQuestion + 1);
-    }
-  };
-
-  const handlePrevClick = () => {
-    if (currentQuestion > 0) {
-      setCurrentQuestion(currentQuestion - 1);
-    }
-  };
-
   return (
-    <div style = {{position: 'relative', top: '100px'}} className="App">
-
-      <h1 className="Basic_Question">Basic Questions</h1>
+    <div className="App">
+      <header className="Basic_Question">Home/Basic Questions</header>
       <br></br>
 
-      {questions[currentQuestion]}
-      <br></br>
-      <Button size="sm" variant="primary" onClick={handlePrevClick} disabled={currentQuestion === 0}>
-        Previous
-      </Button>
-      <Button size="sm" variant="primary" onClick={handleNextClick} disabled={currentQuestion === questions.length - 1}>
-        Next
-      </Button>
-      <br></br>
+      <Q1></Q1>
+      <Q2></Q2>
+      <Q3></Q3>
+      <Q4></Q4>
+      <Q5></Q5>
+      <Q6></Q6>
+      <Q7></Q7>
+      <Q8></Q8>
 
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/views/Detail_Questions.tsx
+++ src/views/Detail_Questions.tsx
@@ -1,50 +1,27 @@
-import React, { useState } from 'react';
-import '../App.css';
-import {Button, Form} from "react-bootstrap";
-import {Q1} from "../DetailQuestions/Q1";
-import {Q2} from "../DetailQuestions/Q2";
-import {Q3} from "../DetailQuestions/Q3";
-import {Q4} from "../DetailQuestions/Q4";
-import {Q5} from "../DetailQuestions/Q5";
-import {Q6} from "../DetailQuestions/Q6";
-import {Q7} from "../DetailQuestions/Q7";
+import { Q1 } from "../DetailQuestions/Q1";
+import { Q2 } from "../DetailQuestions/Q2";
+import { Q3 } from "../DetailQuestions/Q3";
+import { Q4 } from "../DetailQuestions/Q4";
+import { Q5 } from "../DetailQuestions/Q5";
+import { Q6 } from "../DetailQuestions/Q6";
+import { Q7 } from "../DetailQuestions/Q7";
 import { Footer } from "../components/Footer";
 
-
 function Detail_Questions(): JSX.Element {
-  
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const questions = [<Q1 />, <Q2 />, <Q3 />, <Q4 />, <Q5 />, <Q6 />, <Q7 />];
-
-  const handleNextClick = () => {
-    if (currentQuestion < questions.length - 1) {
-      setCurrentQuestion(currentQuestion + 1);
-    }
-  };
-
-  const handlePrevClick = () => {
-    if (currentQuestion > 0) {
-      setCurrentQuestion(currentQuestion - 1);
-    }
-  };
-
   return (
-    <div style = {{position: 'relative', top: '100px'}}className="App">
-
-      <h1 className="Detail_Question">Detail Questions</h1>
-      <br></br>
-      
-      {questions[currentQuestion]}
-      <br></br>
-      <Button variant="primary" onClick={handlePrevClick} disabled={currentQuestion === 0}>
-        Previous
-      </Button>
-      <Button variant="primary" onClick={handleNextClick} disabled={currentQuestion === questions.length - 1}>
-        Next
-      </Button>
+    <div className="App">
+      <header className="Detail_Question">Home/Detail Questions</header>
       <br></br>
 
-      <Footer/>
+      <Q1></Q1>
+      <Q2></Q2>
+      <Q3></Q3>
+      <Q4></Q4>
+      <Q5></Q5>
+      <Q6></Q6>
+      <Q7></Q7>
+
+      <Footer />
     </div>
   );
 }
