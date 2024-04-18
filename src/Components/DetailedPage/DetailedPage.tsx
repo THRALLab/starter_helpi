2c38c2340b6ac4e4ef85a81dc8dd7f195723c48f
Michael Lutz <mikelutz2400@gmail.com>
2024-04-18 15:06:57 -0400

    carousel for detailed questions

--- src/Components/DetailedPage/DetailedPage.tsx
+++ src/Components/DetailedPage/DetailedPage.tsx
@@ -1,24 +1,40 @@
 import React from 'react'
 import './DetailedPage.css'
-import { Card, Form } from 'react-bootstrap'
+import { Card, Form, Carousel } from 'react-bootstrap'
 
 export function DetailedPage() {
+    const questions = ['What is your name?', 'What is your age?', 'What is your?'];
+    const [index, setIndex] = React.useState(0);
+    let questionKey = 0;
+    const handleSelect = (selectedIndex: number) => {
+        setIndex(selectedIndex);
+    };
     return (
         <div className='page-container'>
             <h1>Detailed Page</h1>
-            <Card className='question-card' style={{ backgroundColor: "var(--light-bg)"}}>
-            <Card.Header as='h4' style={{color:'white', padding: '1rem 0'}}>
-                Question 1
-            </Card.Header>
-            <Card.Body>
-                <Form>
-                    <Form.Group style={{ textAlign: 'left' }}>
-                        <Form.Label style={{ color: 'white' }}>Answer</Form.Label>
-                        <Form.Control as='textarea' rows={3} placeholder='Enter your answer' />
-                    </Form.Group>
-                </Form>
-            </Card.Body>
-        </Card>
+            <Card className='question-card' style={{ backgroundColor: "var(--light-bg)" }}>
+                <Card.Header as='h4' style={{ color: 'white', padding: '1rem 0' }}>
+                    Question {index + 1}
+                </Card.Header>
+                <Card.Body>
+                    <Carousel activeIndex={index} onSelect={handleSelect} interval={null}>
+                        {questions.map((question) => (
+                            <Carousel.Item key={questionKey++} style={{height:'20rem'}}>
+                                    <h1 style={{color: 'white'}}>
+                                        {question}
+                                    </h1>
+                                    <Form style={{ width:'50%' }}>
+                                        <Form.Group style={{ textAlign: 'left' }}>
+                                        <Form.Label>Your Answer</Form.Label>
+                                            <Form.Control as='textarea' rows={3} placeholder='Enter your answer' />
+                                        </Form.Group>
+                                    </Form>
+                            </Carousel.Item>
+                        )
+                        )}
+                    </Carousel>
+                </Card.Body>
+            </Card>
         </div >
     )
 
