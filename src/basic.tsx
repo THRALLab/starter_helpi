22b87af28213fbb4a4b28915702c8264d14d27dc
Olive Odida <oodida@udel.edu>
2024-04-18 17:05:10 -0400

    fixed additional basic questions

--- src/basic.tsx
+++ src/basic.tsx
@@ -39,7 +39,39 @@ function Basic() {
 		{
 			question: "7. Would you like to work in-person, hybrid, or virtual?",
 			options: ["in-person", "hybrid", "virtual", "no-preference"]
-		}
+		},
+		{
+			question:'8. What time of day are you most productive?',
+			options: ['early morning (5am-7am)','mid-morning (8am-11am)','afternoon (12pm-5pm)','night (after 6pm)'],
+		  },
+		  {
+			question:'9. What type of company environment are you looking for?',
+			options: ['high-stress, high salary', 'low stress, low salary'],
+		  },
+		  {
+			question:'10. What is the highest level of education that you would like to complete?',
+			options: ['high school/GED','college','Masters or PhD','trade school'],
+		  },
+		  /*{
+			question:'11. On a scale from 1 to 10, how much do you value a larger salary?',
+			options: [''],
+		  }
+		  {
+			question:'12. On a scale from 1 to 10, how good are you at time management?',
+			options:[''],
+		  },*/
+		  {
+			question:'13. How many hours would you like to work per week?',
+			options:['>25','25-30','30-35','35+'],
+		  },
+		  {
+			question:'14. Do you enjoy learning on a constant basis?',
+			options:['yes','no'],
+		  },
+		  {
+			question:'15. Do you enjoy working on a team?',
+			options:['yes','no','sometimes'],
+		  },
 	];
 	return (
 		<div>
