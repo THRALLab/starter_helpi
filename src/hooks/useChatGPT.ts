51003752760ba6ec956377ca81202fd08237e4db
Hairum Qureshi <hqureshi@udel.edu>
2024-04-18 16:29:25 -0400

    formatted ChatGPT's response so it's easier t oread

--- src/hooks/useChatGPT.ts
+++ src/hooks/useChatGPT.ts
@@ -5,14 +5,27 @@ interface Tools {}
 export default function useChatGPT(): Tools {
 	const API_KEY: string | null = localStorage.getItem("MYKEY");
 
-	async function test(openai: OpenAI) {
-		const stream = await openai.chat.completions.create({
-			model: "gpt-3.5-turbo",
-			messages: [{ role: "user", content: "Hello!" }],
-			stream: true
-		});
-		for await (const part of stream) {
-			console.log(part.choices[0].delta);
+	async function callAPI(openai: OpenAI) {
+		let response = "";
+		try {
+			const stream = await openai.chat.completions.create({
+				model: "gpt-3.5-turbo",
+				messages: [
+					{
+						role: "user",
+						content: "Hello!"
+					}
+				],
+				stream: true
+			});
+			for await (const part of stream) {
+				if (part.choices[0].delta.content !== undefined) {
+					response += part.choices[0].delta.content;
+				}
+			}
+			console.log(response);
+		} catch (error) {
+			console.log(error);
 		}
 	}
 
@@ -21,7 +34,7 @@ export default function useChatGPT(): Tools {
 			apiKey: JSON.parse(API_KEY),
 			dangerouslyAllowBrowser: true
 		});
-		test(openai);
+		callAPI(openai);
 	} else {
 		console.log("Please make sure you've entered your API key");
 	}
