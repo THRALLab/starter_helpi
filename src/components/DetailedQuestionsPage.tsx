662494aa0875f48b0f7337f6d23e80bcb09bc919
rubina <srubina886@gmail.com>
2024-04-18 11:56:56 -0400

    working buttons

--- src/components/DetailedQuestionsPage.tsx
+++ src/components/DetailedQuestionsPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Navigate } from 'react-router-dom';
-import { Buttons } from './Buttons';
 
 
 export function DetailedQuestionsPage(): JSX.Element {
