1878a36e124eb4b87cc5889e64c710c45ef7e87d
rubina <srubina886@gmail.com>
2024-04-20 23:27:14 -0400

    improved user interface

--- src/components/BasicQuestionsPage.tsx
+++ src/components/BasicQuestionsPage.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import { Navigate } from 'react-router-dom';
 import Button from '@mui/joy/Button';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import Typography from '@mui/joy/Typography';
+import Input from '@mui/joy/Input';
 
 
 export function BasicQuestionsPage(): JSX.Element {
@@ -42,6 +46,33 @@ export function BasicQuestionsPage(): JSX.Element {
 				{ answerText: 'Option 4' },
 			],
 		},
+        {
+			questionText: 'Question 5',
+			answerOptions: [
+				{ answerText: 'Option 1' },
+				{ answerText: 'Option 2' },
+				{ answerText: 'Option 3' },
+				{ answerText: 'Option 4' },
+			],
+		},
+        {
+			questionText: 'Question 6',
+			answerOptions: [
+				{ answerText: 'Option 1' },
+				{ answerText: 'Option 2' },
+				{ answerText: 'Option 3' },
+				{ answerText: 'Option 4' },
+			],
+		},
+        {
+			questionText: 'Question 7',
+			answerOptions: [
+				{ answerText: 'Option 1' },
+				{ answerText: 'Option 2' },
+				{ answerText: 'Option 3' },
+				{ answerText: 'Option 4' },
+			],
+		}
 	];    
 
     const [goToHomePage, setGoToHomePage] = React.useState(false);
@@ -91,48 +122,86 @@ export function BasicQuestionsPage(): JSX.Element {
     return (
         <div>
             <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand>CareerFinder4U</Navbar.Brand>
-             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="me-auto">
-              <Nav.Link onClick={handleGoToHomePage}>Home</Nav.Link>
-              <Nav.Link onClick={() => {setGoToDetailedQuestionsPage(true)}}>Detailed Questions Page</Nav.Link>
-                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+                <Container>
+                    <Navbar.Brand>CareerFinder4U</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                <Nav.Link onClick={handleGoToHomePage}>Home</Nav.Link>
+                <Nav.Link onClick={() => {setGoToDetailedQuestionsPage(true)}}>Detailed Questions Page</Nav.Link>
+                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                    Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                </NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
             <p></p>
             <h1 className='padding3'>Basic Questions Page</h1>
             <p className="text-muted">This basic career assessment is hand crafted to help comprehend preferences and strengths that you have and which specific careers they are best suited for. You'll gain insights into the types of careers and opportunities that may suit you best. Coming soon. </p>
-            <input
-                type="text"
-                value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
-            />
-            <Button onClick={handleClearText}>Reset</Button>
-            {!displayFinalResults && <div>
-                <p>{currentQuestion+1}/{questions.length}</p>
-                <p>{questions[currentQuestion].questionText}</p>
-                <p>{questions[currentQuestion].answerOptions.map((answerOption) => <p>{answerOption.answerText}</p>)}</p>
-            </div>}
-            {(!displayFinishButton && !displayFinalResults)&& <div><Button onClick={handleCurrentQuestion}>Next Question</Button></div>}
             
-            {displayFinalResults && <div>Final Results</div>}
-            {(displayFinishButton) && <div><Button color='success' onClick={handleDisplayFinalResults}>Finish & Get Results</Button></div>}
-            <p></p>
+            <div
+                style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '40vh', // Adjust this value according to your layout
+                padding: '30vh'
+            }}>
+                <Card variant="plain" sx={{ width: 1000, height: 400}}// Adjust this value according to your layout
+                >
+                    <CardContent>
+                        <Typography level="title-md">Question {currentQuestion+1}/{questions.length}</Typography>
+                        <Typography style={{alignItems: 'center', padding: '5vh'}}>{!displayFinalResults && <div>
+                        <p>{questions[currentQuestion].questionText}</p>
+                        <div style={{paddingBottom: '1vh', display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center'}}>
+                            {/*<input
+                            type="text"
+                            value={inputText}
+                            onChange={(e) => setInputText(e.target.value)}
+                        />*/}
+                            <Input sx={{width: '500px', height: '75px', display: 'flex',
+                                    justifyContent: 'center',
+
+                                    alignItems: 'center',
+                                    '--Input-focusedInset': 'var(--any, )',
+                                    '--Input-focusedThickness': '0.25rem',
+                                    '--Input-focusedHighlight': 'rgba(13,110,253,.25)',
+                                    '&::before': {
+                                      transition: 'box-shadow .15s ease-in-out',
+                                    },
+                                    '&:focus-within': {
+                                      borderColor: '#86b7fe',
+                                    }}} 
+                                    variant="outlined" placeholder="Type in here…" value={inputText} 
+                                    required
+                                    onChange={(e) => {
+                                    setInputText(e.target.value);
+                                    
+                        }} />
+                        </div>
+                        <div style={{paddingTop: '1vh'}}><Button onClick={handleClearText}>Reset</Button></div>
+                        
+                        </div>}
+                        {(!displayFinishButton && !displayFinalResults)&& <div style={{padding: '12vh'}}><Button onClick={handleCurrentQuestion}>Next Question</Button></div>}
             
+                        {displayFinalResults && <div>Final Results</div>}
+                        {(displayFinishButton) && <div style={{padding: '12vh'}}><Button color='success' onClick={handleDisplayFinalResults}>Finish & Get Results</Button></div>}
+                        <p></p>
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>  
         </div>
     );
 }
--- src/components/DetailedQuestionsPage.tsx
+++ src/components/DetailedQuestionsPage.tsx
@@ -1,17 +1,105 @@
-import React, { useState } from 'react';
-import { Form } from 'react-bootstrap';
-
+import React from 'react';
 import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import { Navigate } from 'react-router-dom';
 import Button from '@mui/joy/Button';
-
-
-
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import Typography from '@mui/joy/Typography';
+import Input from '@mui/joy/Input';
+import Divider from '@mui/joy/Divider';
 
 export function DetailedQuestionsPage(): JSX.Element {
-    const [goToHomePage, setGoToHomePage] = useState(false);
-    const [inputText, setInputText] = useState('');
+  const questions = [
+		{
+			questionText: 'Question 1',
+			answerOptions: [
+				{ answerText: 'Option 1' },
+				{ answerText: 'Option 2' },
+				{ answerText: 'Option 3'},
+				{ answerText: 'Option 4'},
+			],
+		},
+		{
+			questionText: 'Question 2',
+			answerOptions: [
+				{ answerText: 'Option 1' },
+				{ answerText: 'Option 2' },
+				{ answerText: 'Option 3' },
+				{ answerText: 'Option 4' },
+			],
+		},
+		{
+			questionText: 'Question 3',
+			answerOptions: [
+				{ answerText: 'Option 1' },
+				{ answerText: 'Option 2' },
+				{ answerText: 'Option 3' },
+				{ answerText: 'Option 4' },
+			],
+		},
+		{
+			questionText: 'Question 4',
+			answerOptions: [
+				{ answerText: 'Option 1' },
+				{ answerText: 'Option 2' },
+				{ answerText: 'Option 3' },
+				{ answerText: 'Option 4' },
+			],
+		},
+        {
+			questionText: 'Question 5',
+			answerOptions: [
+				{ answerText: 'Option 1' },
+				{ answerText: 'Option 2' },
+				{ answerText: 'Option 3' },
+				{ answerText: 'Option 4' },
+			],
+		},
+        {
+			questionText: 'Question 6',
+			answerOptions: [
+				{ answerText: 'Option 1' },
+				{ answerText: 'Option 2' },
+				{ answerText: 'Option 3' },
+				{ answerText: 'Option 4' },
+			],
+		},
+        {
+			questionText: 'Question 7',
+			answerOptions: [
+				{ answerText: 'Option 1' },
+				{ answerText: 'Option 2' },
+				{ answerText: 'Option 3' },
+				{ answerText: 'Option 4' },
+			],
+		}
+	];    
+
+    const [goToHomePage, setGoToHomePage] = React.useState(false);
+    const [inputText, setInputText] = React.useState('');
     const [goToBasicQuestionsPage, setGoToBasicQuestionsPage] = React.useState(false);
+    const [displayFinishButton, setDisplayFinishButton] = React.useState(false);
+    const [displayFinalResults, setDisplayFinalResults] = React.useState(false);
+
+    const [currentQuestion, setCurrentQuestion] = React.useState(0);
+
+    const handleCurrentQuestion = () => {
+        const currentQuestionIndex = currentQuestion + 1;
+        
+        if (currentQuestionIndex < questions.length) {
+            setCurrentQuestion(currentQuestionIndex)
+        }
+
+        if (currentQuestionIndex === questions.length - 1) {
+            setDisplayFinishButton(true);
+        }
+    
+    }
+
+    const handleDisplayFinalResults = () => {
+        setDisplayFinalResults(true);
+        setDisplayFinishButton(false);
+    }
 
     const handleClearText = () => {
         setInputText('');
@@ -53,12 +141,61 @@ export function DetailedQuestionsPage(): JSX.Element {
             <p></p>
             <h1 className='padding3'>Detailed Questions Page</h1>
             <p className="text-muted">The detailed career assessment goes in depth to plan out your professional preferences, skills, and motivators. Completing this assessment will provide you with a complex overview of career paths that align with your detailed profile. Coming soon. </p>
-            <Form.Control
-                type="text"
-                value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
-            />
-            <Button variant="solid" onClick={handleClearText}>Reset</Button>
+            <div
+                style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '40vh', // Adjust this value according to your layout
+                padding: '30vh'
+            }}>
+                <Card variant="plain" sx={{ width: 1000, height: 400}}// Adjust this value according to your layout
+                >
+                    <CardContent>
+                        <Typography level="title-md">Question {currentQuestion+1}/{questions.length}</Typography>
+                        <Typography style={{alignItems: 'center', padding: '5vh'}}>{!displayFinalResults && <div>
+                        <p>{questions[currentQuestion].questionText}</p>
+                        <Divider />
+                        <div style={{paddingBottom: '1vh', display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center'}}>
+                            {/*<input
+                            type="text"
+                            value={inputText}
+                            onChange={(e) => setInputText(e.target.value)}
+                        />*/}
+                            <Input sx={{width: '500px', height: '75px', display: 'flex',
+                                    justifyContent: 'center',
+
+                                    alignItems: 'center',
+                                    '--Input-focusedInset': 'var(--any, )',
+                                    '--Input-focusedThickness': '0.25rem',
+                                    '--Input-focusedHighlight': 'rgba(13,110,253,.25)',
+                                    '&::before': {
+                                      transition: 'box-shadow .15s ease-in-out',
+                                    },
+                                    '&:focus-within': {
+                                      borderColor: '#86b7fe',
+                                    }}} 
+                                    variant="outlined" placeholder="Type in here…" value={inputText} 
+                                    required
+                                    onChange={(e) => {
+                                    setInputText(e.target.value);
+                                    
+                        }} />
+                        </div>
+                        <div style={{paddingTop: '1vh'}}><Button onClick={handleClearText}>Reset</Button></div>
+                        
+                        </div>}
+                        {(!displayFinishButton && !displayFinalResults)&& <div style={{padding: '12vh'}}><Button onClick={handleCurrentQuestion}>Next Question</Button></div>}
+            
+                        {displayFinalResults && <div>Final Results</div>}
+                        {(displayFinishButton) && <div style={{padding: '12vh'}}><Button color='success' onClick={handleDisplayFinalResults}>Finish & Get Results</Button></div>}
+                        <p></p>
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>  
         </div>
     );
 }
