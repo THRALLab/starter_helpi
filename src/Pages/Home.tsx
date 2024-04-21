f10204156c5ddc36f3590b1095001a60f49e8764
Joey <joeydare@comcast.net>
2024-04-21 19:33:38 -0400

    Basic and Detailed text added and image to center box

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,6 +4,7 @@ import BrainIcon from './modifiedBrainIcon.svg';
 import homeIcon from './house.svg';
 import arrowIcon from './arrow.svg';
 import './Pages.css';
+import telescopeManImage from './telescope-man.png';
 
 interface HomeProp {
     handlePage: (page: string) => void;
@@ -19,8 +20,18 @@ const Home: React.FC<HomeProp> = ({ handlePage }) => {
         </div>
         </header>
         <div className="home-center">
+        <div className="background-image" style={{backgroundImage: `url(${telescopeManImage})`}}></div>
             <h1 className="center-title">Start your future here</h1>
             <p className="center-left">Student-made career test<br></br>powered through the use of AI</p>
+            <p className="basic-page-description">
+                    The Basic Page provides a quick overview of potential career paths based on your responses to fundamental questions. 
+                    It's a great starting point for exploring your interests and skills.
+                </p>
+                <p className="detailed-page-description">
+                    The Detailed Page offers a more in-depth insight into specific career options by asking more detailed into 
+                    what you really want out of a career Dive deeper into each career path to make 
+                    well-informed decisions about your future.
+                </p>
             <Button className="basic-button" onClick={() => handlePage('Basic')}>Basic Questions<img src={arrowIcon} alt="arrow" className="arrow-icon" /></Button>
                 <Button className="detailed-button" onClick={() => handlePage('Detailed')}>Detailed Questions<img src={arrowIcon} alt="arrow" className="arrow-icon" /></Button>
         </div>
