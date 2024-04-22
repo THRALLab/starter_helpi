ed36c1b55ed68af864ef7ed6b757da33f2de2906
Hairum Qureshi <hqureshi@udel.edu>
2024-04-22 17:40:03 -0400

    styled modal

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,7 +1,19 @@
+import "./modal.css";
+
 export default function Modal() {
 	return (
-		<div>
-			<h1>MODAL</h1>
+		<div className="modal">
+			<div className="modal-content">
+				<p>
+					Congrats! You've completed all of the questions! Click cancel if you
+					would like to go back and review your answer choices once more;
+					otherwise, click 'Get Results!'.
+				</p>
+				<div className="buttonContainer">
+					<button>Cancel</button>
+					<button>Get Results!</button>
+				</div>
+			</div>
 		</div>
 	);
 }
