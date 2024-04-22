7d663018d919465cc37b7b5b48435214a51b9281
Hairum Qureshi <hqureshi@udel.edu>
2024-04-22 11:21:50 -0400

    fixed capitalization in app.css file name

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./app.css";
+import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Basic from "./basic";
 import Detailed from "./detailed";
