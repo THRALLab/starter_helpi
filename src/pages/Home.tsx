f29799d275951a58a6ba958cd004471f00362bb0
Sophia Romero <siromero@udel.edu>
2024-04-23 12:16:22 -0400

    Adding touches to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,27 +8,30 @@ interface HomeProp {
 const home: React.FC<HomeProp> = ({handlePage}) => {
     return (
     <div>
-        <h1 style={{ textAlign: 'center'}}> Shore Path</h1>
+        <div className="bigBox">
+        <h1>ShorePath</h1>
+        <h2>In the ocean of opportunities find which career suits you best</h2>
         <div className="container">
-            <div className="section">
-                <h2 className="basicHeader">Basic</h2>
+            <div className="box">
+                <h2 className="basicHeader">Basic Quiz</h2>
                 <p className="basicParagraph">
-                    The basic assessment quiz is a short questionaire designed
-                    to gauge your knowledge and skills with simple and clear
+                    The Basic Quiz is a short questionaire designed
+                    to gauge your knowledge with simple
                     questions. With immediate feedback, you will know which
                     career is right for you in a matter of a few minutes!{" "}
                 </p>
                 <button className="rounded-button blue-gradient" onClick={() => handlePage('Basic')}> Start Basic Quiz</button>
             </div>
-            <div className="section">
-                <h2 className="detailedHeader">Detailed</h2>
+            <div className="box">
+                <h2 className="detailedHeader">Detailed Quiz</h2>
                 <p className="detailedParagraph">
-                    The Detailed assessment will consist of more questions and
+                    The Detailed Quiz is a longer assessment consisting of more questions with more detailed choices. It
                     will give you a more accurate and detailed assesment of what
                     carreer you should choose.
                 </p>
                 <button className="rounded-button blue-gradient" onClick={() => handlePage('Detailed')}> Start Detailed Quiz</button>
             </div>
+        </div>
         </div>
     </div>
     );
