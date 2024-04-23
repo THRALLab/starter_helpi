7dcdadc49072ef101b10e59bc5ff90a039b39738
OLIVIA FOLIIARD <FOLLIARD@UDEL.EDU>
2024-04-22 21:40:43 -0400

    fixed button visibility

--- src/basicDetailedButton.tsx
+++ src/basicDetailedButton.tsx
@@ -1,34 +1,41 @@
 import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./basicDetailedButton.css"
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 
 function BasicDetailedButtons() {
     const [clicked, setClicked] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        setClicked(false);
+    }, [location.pathname]);
 
     const handleClick = () => {
         setClicked(true);
     };
 
+    const isHome = location.pathname === "/";
+
     return (
         <div>
-           {!clicked && (<div>
+           {isHome && !clicked && (<div>
                 <Link to="/basic">
                     <button onClick={handleClick}>Basic Quiz</button>
                 </Link>
             </div>)}
-            {!clicked && (<p>
+            {isHome && !clicked && (<p>
             <p>This is a 10 - 15 minute quiz aimed to gauge</p>
             <p>interests and skills in order to provide suggestions</p>
             <p>and information on potential careers.</p></p>)}
 
-            {!clicked && (<div>
+            {isHome && !clicked && (<div>
                 <Link to="/detailed">
-                    <div><button>Detailed Quiz</button></div>
+                    <button onClick={handleClick}>Detailed Quiz</button>
                 </Link>
             </div>)}
-            {!clicked && (<p>
+            {isHome && !clicked && (<p>
                 <p>This is a 30 - 35 minute quiz aimed to</p>
                 <p>understand your goals and passions in</p>
                 <p>order to provide potential careers.</p>
