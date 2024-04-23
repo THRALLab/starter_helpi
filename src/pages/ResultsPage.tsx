a67a728c315d1483fed49b003fa4f586e28cfa36
Coleman <cowalsh@udel.edu>
2024-04-23 12:29:25 -0400

    established resultspage files

---
+++ src/pages/ResultsPage.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import './resultsPage.css';
+
+
+const ResultsPage = () => {
+
+
+    return(
+        <>
+        
+        </>
+    )
+};
