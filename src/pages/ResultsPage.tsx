9bc90c302f58642fb5ce8ab4c1253de4fff8f512
Coleman <cowalsh@udel.edu>
2024-04-24 00:52:30 -0400

    established structure of results page

--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -8,7 +8,26 @@ const ResultsPage = () => {
 
     return(
         <>
-        
+        <div className="mainCareer">
+                <h6>Your ideal career is...</h6>
+                <h5>UD Parking Attendant</h5>
+                <p>You can tell it's an aspen because of the way it is.</p>
+        </div>
+        <div className="subCareers">
+            <div className="career-desc">
+                        <h3>Option 2</h3>
+                        <p>This is the description of career option 2</p>
+            </div>
+            <div className="career-desc">
+                        <h3>Option 3</h3>
+                        <p>This is the description of career option 3</p>
+            </div>
+            <div className="career-desc">
+                        <h3>Option 4</h3>
+                        <p>This is the description of career option 4</p>
+            </div>
+        </div>
         </>
     )
 };
+export default ResultsPage;
