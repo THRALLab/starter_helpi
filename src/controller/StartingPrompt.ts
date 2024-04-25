3024f470de953c9cadca880a0abbbbe21c09d80c
IsaacWeber1 <isaacgweber@gmail.com>
2024-04-24 21:23:26 -0400

    messing with prompt

--- src/controller/StartingPrompt.ts
+++ src/controller/StartingPrompt.ts
@@ -157,7 +157,7 @@ userStatus +
 
         `Your job is to ask for more information in order to obtain the necessary information to meet their specific needs.\n` +
         `Generate these questions in sets rather than individually to enhance efficiency and reduce the number of interactions required.\n` +
-        `Each set of questions should be independent of answers to previous sets to ensure that no answer influences the subsequent ones within the same set.\n\n` +
+        `Each set of questions should be independent of answers to previous sets to ensure that no answer influences the subsequent ones within the same set.\n\n\n` +
 
 
         `The questions should be structured as follows:\n` +
@@ -182,16 +182,39 @@ userStatus +
         `    options: string[]\n` +
         `}\n\n` +
 
-
         `Here are the instructions for how to utilize each of the five question types:\n` +
-        `MC_SINGLE_RESPONSE, MC_MULTI_RESPONSE, TEXT_RESPONSE, SLIDER_RESPONSE, USER_RANKING.\n\n` +
+
+        `MC_SINGLE_RESPONSE:\n` +
+        `This types is a single response multiple choice question.\n` +
+        `You should use the MC_SINGLE_RESPONSE type when you want the user to choose only one of the given options.\n\n` +
+
+        `MC_MULTI_RESPONSE:\n` +
+        `This type is a multiple response multiple choice question.\n` +
+        `You should use the MC_MULTI_RESPONSE type when you want to gather all applicable user preferences.\n` +
+        `The MC_MULTI_RESPONSE type also has a special feature, unique to this question type:\n` +
+        `   If you list 'Other (click to specify)' as one of the options in the question, the user will have the ability to select this option and type a custom answer choice.\n\n` +
+
+        `TEXT_RESPONSE:\n` +
+        `This type is a text response question, similar to short/long answer question types.\n` +
+        `You should use the TEXT_RESPONSE type when you want the user to type a custom answer to the given prompt.\n` +
+        `The options field a TEXT_RESPONSE question should be an empty list since the type does not require options.\n\n` +
+
+        `SLIDER_RESPONSE:\n` +
+        `This type is a slider question where the user will be instructed to choose a value between 1 and 100.\n` +
+        `You should use the SLIDER_RESPONSE type when you want to gauge how strongly the user feels about something.\n` +
+        `The options field a SLIDER_RESPONSE question should be an empty list since the type does not require options.\n\n` +
+
+        `USER_RANKING:\n` +
+        `This type is a custom ranking question where the user will be asked to rank the question options in order of preference.\n` +
+        `Specify in the question prompt the order in which you would like the user to consider the options, such as from most to least preferred.\n` +
+        `Use the USER_RANKING type when you want to understand the user's comparative preferences among a list of options.\n` +
+        `This can help in determining priorities or preferences without the constraints of other types of questions that limit the responses to single or multiple choices.\n\n\n` +
 
 
         `Finally, after gathering all necessary information through the quiz, compile a final report. This report will: \n` +
         `- Provide a personalized summary of the user's career interests and potential paths based on their quiz responses.\n` +
         `- Offer actionable steps and long-term strategies tailored to the user's current educational or career stage.\n` +
         `- Include interactive elements allowing the user to explore more detailed information about each recommended career path.\n\n` +
-
 
         `This final report aims to be a comprehensive guide that assists users in making informed decisions about their career paths, whether they are just beginning to explore or are considering a change.\n`
     )
