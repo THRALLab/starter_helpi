220c4d7dd74297a774f893dcae6491368cfed8d0
jacobwilbe <jacobwilbe@users.noreply.github.com>
2024-04-24 23:15:41 -0400

    finally completed logic for saving page state in session storage, and updating object array to send to openai

--- src/Components/BasicPage/BasicPage.tsx
+++ src/Components/BasicPage/BasicPage.tsx
@@ -1,22 +1,26 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Accordion from 'react-bootstrap/Accordion';
 import './BasicPage.css'
 import RangeSlider from "./Rangeslider";
+import { Button } from 'react-bootstrap';
+
+interface QuestionData{
+    question:string;
+    answer: string;
+}
+
+const index: Record<string, number> = {
+    "1. I enjoy solving complex problems:" : 0,
+    "2. I prefer working in a team rather than alone: ": 1,
+    "3. I am comfortable with public speaking and presenting ideas to others:": 2,
+    "4. I find it fulfilling to help others and make a positive impact on their lives:" : 3,
+    "5. I am interested in technology and staying updated with the latest trends:": 4,
+    "6. I prefer a job that allows for a flexible schedule and the possibility of remote work:": 5,
+    "7. I am more creative than analytical:": 6
+}
 
 
-
-
-
-export function BasicPage() {
-    interface QuestionAnswer{
-        question:string;
-        answer: string;
-    }
-
-    const [rangeSlider, setRangeSliderValue] = useState("3");
-  
-
-    const questions: string[] = 
+const questions: string[] = 
     [
     "1. I enjoy solving complex problems:",
     "2. I prefer working in a team rather than alone: ", 
@@ -27,16 +31,46 @@ export function BasicPage() {
     "7. I am more creative than analytical:"
     ]
 
+let pageData = Array.from(questions, (question: string) => ({ question: question, answer: "Neither Agree nor Disagree" })) as QuestionData[];
+const saveDetailedDataKey = "BASIC_DATA";
+const currData = sessionStorage.getItem(saveDetailedDataKey);
+if (currData !== null) {
+    pageData = JSON.parse(currData) as QuestionData[];
+}
 
-    const [responses, setResponses] = useState<QuestionAnswer[]>(questions.map(question => ({question, answer: rangeSlider})));
+const rangeValues: Record<string, string> = {
+    1: "Disagree",
+    2: "Somewhat Disagree",
+    3: "Neither Agree nor Disagree",
+    4: "Somewhat Agree",
+    5: "Agree"
+  };
 
-    const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>, question: string, newValue: string) => {
-        setResponses(responses.map(resp =>
-          resp.question === question ? { ...resp, answer: newValue } : resp
-        ));
-        setRangeSliderValue(event.target.value)
-        console.log(responses);
-      };
+const reverseMap: Record<string, string>= { 
+    "Disagree": "1",
+    "Somewhat Disagree": "2",
+    "Neither Agree nor Disagree": "3",
+    "Somewhat Agree": "4",
+    "Agree": "5"
+}
+
+
+export function BasicPage() {
+
+    const [questionData, setQuestionData] = React.useState(pageData);
+    const [isFinished, setIsFinished] = React.useState(questionData.every((question) => question.answer.length > 0));
+
+    const handleSliderChange = (question: string, resp: string) => {
+        const newAnswer = rangeValues[resp]
+        let data = [...questionData];
+        data[index[question]] = { question: question, answer: newAnswer};
+        setQuestionData(data);
+        // Check if all questions have been answered
+        setIsFinished(data.every((question) => question.answer.length > 0));
+            // Save data to local storage
+        sessionStorage.setItem(saveDetailedDataKey, JSON.stringify(data));
+        console.log(data)
+    };
 
     let eventKey = 0;
     return (
@@ -47,13 +81,20 @@ export function BasicPage() {
                     {questions.map((question: string) => (
                         <Accordion.Item key={eventKey} eventKey={(eventKey++).toString()} className="item">
                             <Accordion.Header className='header'>{question}</Accordion.Header>
-                            <Accordion.Body className='body'>
-                                <RangeSlider question={question} handleChange={handleSliderChange} />
+                            <Accordion.Body className='body'>  
+                                <RangeSlider 
+                                    question={question} 
+                                    handleChange={handleSliderChange}
+                                    rangeValues={rangeValues} 
+                                    key={question}
+                                    numresp={reverseMap[questionData[index[question]].answer]}
+                                />
                             </Accordion.Body>
                         </Accordion.Item>
                     )
                     )}
                 </Accordion>
+                <Button style={{ backgroundColor: '#6923ff', borderColor: '#6923ff' }} disabled={!isFinished}>Submit</Button>
             </div>
         </div>
     )
