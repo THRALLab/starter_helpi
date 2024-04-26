7773ea6c647eaf281b84dcab8e55aa92b41aefe2
Hairum Qureshi <hqureshi@udel.edu>
2024-04-26 08:49:32 -0400

    removed unused API code in the App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,6 @@ import Navigation from "./navbar";
 import Footer from "./Footer";
 import BasicDetailedButtons from "./basicDetailedButton";
 
-//local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
-let keyData = "";
-const saveKeyData = "MYKEY";
-const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
-if (prevKey !== null) {
-	keyData = JSON.parse(prevKey);
-}
-
 function App() {
 	return (
 		<Router>
