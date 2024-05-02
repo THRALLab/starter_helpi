7e8fc96cba5dd39c8b5fb57314a4c3843901f4c2
CARTER MCCABE <cfmccabe@udel.edu>
2024-05-02 13:04:17 -0400

    final fix

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link} from 'react-router-dom';
 // import { useNavigate } from 'react-router-dom';
 import './App.css';
 import './HomePage.css';
