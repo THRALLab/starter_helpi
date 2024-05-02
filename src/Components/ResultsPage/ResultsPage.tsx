660e2ada71c4e58a698c204d912f1cb731d3e6af
Colin Parsons <colinparsons5@gmail.com>
2024-05-02 15:32:54 -0400

    Working Loading Animation, not updated to reflect API request

--- src/Components/ResultsPage/ResultsPage.tsx
+++ src/Components/ResultsPage/ResultsPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
+import './ResultsPage.css'
 
 interface QuestionData {
     question: string;
@@ -12,22 +13,26 @@ export function ResultsPage({basicQuestionData, detailQuestionData} : {basicQues
     
     return (
         <>
-            <div className='main-component'>
-                <h1>ResultsPage</h1>
-                <div>
-                    <Row>
-                        <div className="spinner-grow text-blurple" style={{ width: "5rem", height: "5rem", marginTop: "2.5rem", marginRight: ".5rem" }} role="status">
-                            <span className="visually-hidden">Loading...</span>
-                        </div>
-                        <div className="spinner-grow text-blurple" style={{ width: "10rem", height: "10rem" }} role="status">
-                            <span className="visually-hidden">Loading...</span>
-                        </div>
-                        <div className="spinner-grow text-blurple" style={{  width: "5rem", height: "5rem", marginTop: "2.5rem", marginLeft: ".5rem" }} role="status">
-                            <span className="visually-hidden">Loading...</span>
-                        </div>
-                    </Row>
-                </div>
+            <div className='d-flex justify-content-center align-items-center' style={{marginBottom: '-15rem'}}>
+                <Row>
+                    <div className="spinner-grow text-blurple" style={{  width: "2.5rem", height: "2.5rem", marginTop: "2.75rem", marginRight: ".5rem", animationDuration: '1.5s' }} role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-blurple" style={{ width: "5rem", height: "5rem", marginTop: "1.5rem", marginRight: ".5rem", animationDuration: '1.5s' }} role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-blurple" style={{ width: "7.5rem", height: "7.5rem", animationDuration: '1.5s' }} role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-blurple" style={{  width: "5rem", height: "5rem", marginTop: "1.5rem", marginLeft: ".5rem", animationDuration: '1.5s' }} role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-blurple" style={{  width: "2.5rem", height: "2.5rem", marginTop: "2.75rem", marginLeft: ".5rem", animationDuration: '1.5s' }} role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                </Row>
             </div>
+            <div className='loading-text'>Your Report will be ready momentarily</div>
         </>
     )
 }
