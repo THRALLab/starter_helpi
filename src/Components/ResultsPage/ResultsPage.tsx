ba1595893ad36fbc36dc9a5f5e2755dbe1d66102
Colin Parsons <colinparsons5@gmail.com>
2024-05-02 15:01:10 -0400

    Testing out loading animation

--- src/Components/ResultsPage/ResultsPage.tsx
+++ src/Components/ResultsPage/ResultsPage.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Row } from 'react-bootstrap';
 
-export function ResultsPage() {
+export function ResultsPage({ page, setPage }: Readonly<{ page: string; setPage: (newPage: string) => void }>) {
     return (
-        <h1>ResultsPage</h1>
+        <>
+            <div className='main-component'>
+                <h1>ResultsPage</h1>
+                <div>
+                    <Row>
+                        <div className="spinner-grow text-blurple" style={{ width: "5rem", height: "5rem", marginTop: "2.5rem", marginRight: ".5rem" }} role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                        <div className="spinner-grow text-blurple" style={{ width: "10rem", height: "10rem" }} role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                        <div className="spinner-grow text-blurple" style={{  width: "5rem", height: "5rem", marginTop: "2.5rem", marginLeft: ".5rem" }} role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </Row>
+                </div>
+            </div>
+        </>
     )
 }
