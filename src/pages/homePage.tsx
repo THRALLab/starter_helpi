7b43cfd0606a0dc1d50f4d237aa5945dbfce0b30
Coleman <cowalsh@udel.edu>
2024-05-03 08:10:48 -0400

    fixed unused import

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import splash from "../images/businessman.png"
-import pie from "../images/piechart.png"
 import './homePage.css';
 import { HomeLink } from "../components/NavbarElements"
 import { Link } from "react-router-dom";
