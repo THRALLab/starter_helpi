3ec68cf7e0ec1eae7c90929a4b8b750878c85b18
Jasnoor Saini <jasnoor.saini@icloud.com>
2024-05-04 19:39:41 -0400

    Added styling to the button to make it clear what it is supposed to do

--- src/components/darkMode.tsx
+++ src/components/darkMode.tsx
@@ -2,20 +2,24 @@ import { useState } from "react";
 
 export const DarkModeToggle = () => {
   const [isDark, setIsDark] = useState(true);
+  const [Label, setLabel] = useState("Dark Mode" || "Light Mode");
 
   const handleToggle = () => {
     setIsDark(!isDark);
+    setLabel(isDark ? "Light Mode" : "Dark Mode");
+    
   };
 
   return (
     <button
       onClick={handleToggle}
       style={{
-        height: "100px",
+        height: "30px",
         width: "100px",
+        borderRadius: "15px",
         backgroundColor: isDark ? "black" : "white",
         color: isDark ? "white" : "black",
       }}
-    ></button> // Added closing tag for the button element
+    >{Label}</button>
   );
 };
