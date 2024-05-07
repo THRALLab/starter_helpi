103a7b0cf5a99240f8b7247c4b7824125c87e333
Hairum Qureshi <hqureshi@udel.edu>
2024-05-06 20:19:55 -0400

    created CSS file for the Results page and applied styling to the main div

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Detailed from "./detailed";
 import Navigation from "./navbar";
 import Footer from "./Footer";
 import BasicDetailedButtons from "./basicDetailedButton";
+import Results from "./Results";
 
 function App() {
 	return (
@@ -15,6 +16,7 @@ function App() {
 				<Routes>
 					<Route path="/basic" Component={Basic} />
 					<Route path="/detailed" Component={Detailed} />
+					<Route path="/results" Component={Results} />
 				</Routes>
 				<Footer />
 			</>
---
+++ src/Results.tsx
@@ -0,0 +1,5 @@
+import "./results.css";
+
+export default function Results() {
+	return <div className="report">Results</div>;
+}
