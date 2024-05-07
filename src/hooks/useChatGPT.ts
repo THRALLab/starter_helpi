661a2792cfde59f415942548242395d8a071ef52
Hairum Qureshi <hqureshi@udel.edu>
2024-05-06 20:05:25 -0400

    saved response data in state

--- src/hooks/useChatGPT.ts
+++ src/hooks/useChatGPT.ts
@@ -27,7 +27,7 @@ export default function useChatGPT(): Tools {
 				messages: [
 					{
 						role: "user",
-						content: `I am looking to generate a detailed and lengthy report catered towards helping a user find a list of 4 careers by name that would closely match with what they've answered given a set of questions. When generating this report, please give a detailed explanation why each career you list may be a good fit for the user. Please also provide alternative paths the user could look into if the given list of potential careers you provide may not be of interest to the user. If any of the questions receive gibberish answers or don't make sense, ignore them. These questions and answers are as follows: \n ${formattedQ_A}`
+						content: `I am looking to generate a detailed report catered towards helping a user find a list of 4 careers by name that would closely match with what they've answered given a set of questions. When generating this report, please give a detailed explanation why each career you list may be a good fit for the user. Please also provide alternative paths the user could look into if the given list of potential careers you provide may not be of interest to the user. If any of the questions receive gibberish answers or don't make sense, ignore them. These questions and answers are as follows: \n ${formattedQ_A}`
 					}
 				],
 				stream: true
@@ -39,7 +39,6 @@ export default function useChatGPT(): Tools {
 			}
 
 			setChat_gptResponse(response);
-			console.log(setChat_gptResponse);
 		} catch (error) {
 			console.log(error);
 		}
