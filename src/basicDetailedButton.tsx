4f54787a590ab4fb451633e7bc0a6af69bbff005
Hairum Qureshi <hqureshi@udel.edu>
2024-05-11 01:56:04 -0400

    redesigned buttons layout and description

--- src/basicDetailedButton.tsx
+++ src/basicDetailedButton.tsx
@@ -1,55 +1,29 @@
-import { Link, useLocation } from "react-router-dom";
-import "./basicDetailedButton.css"
-import {useState, useEffect} from "react";
-
+import { useNavigate } from "react-router-dom";
+import "./basicDetailedButton.css";
+import { useState } from "react";
 
 function BasicDetailedButtons() {
-    const [clicked, setClicked] = useState(false);
-    const location = useLocation();
+	const navigate = useNavigate();
 
-    useEffect(() => {
-        setClicked(false);
-    }, [location.pathname]);
-
-    const handleClick = () => {
-        setClicked(true);
-    };
-
-    const isHome = location.pathname === "/";
-
-    return (
-        <div>
-            <div className="button-container">
-                {isHome && !clicked && (
-                <div>
-                    <Link to="/basic">
-                        <button className="basic-button" onClick={handleClick}>Basic Quiz</button>
-                    </Link>
-                </div>
-                )}
-
-                {isHome && !clicked && (
-                <div>
-                    <Link to="/detailed">
-                        <button className="detailed-button" onClick={handleClick}>Detailed Quiz</button>
-                    </Link>
-                </div>
-                )}
-            </div>
-
-        <div className="par-container">{isHome && !clicked && (
-            <p className="par-basic">
-                This is a 10 - 15 minute quiz aimed to gauge<br></br>
-                interests and skills to provide suggestions<br></br>
-                and information on potential careers.
-            </p>)}
-            {isHome && !clicked && (
-            <p className="par-detailed">
-            This is a 30 - 35 minute quiz aimed to<br></br>
-            understand your goals and passions in<br></br>
-            order to provide potential careers.
-        </p>)}
-        </div>
-    </div>
-)}
+	return (
+		<>
+			<div className="buttonContainer">
+				<div className="basicBtnContainer">
+					<button onClick={() => navigate("/basic")}>Basic Quiz</button>
+					<p>
+						This is a 10 - 15 minute quiz aimed to gauge interests and skills to
+						provide suggestions and information on potential careers.
+					</p>
+				</div>
+				<div className="detailedBtnContainer">
+					<button onClick={() => navigate("/detailed")}>Detailed Quiz</button>
+					<p>
+						This is a 30 - 35 minute quiz aimed to understand your goals and
+						passions in order to provide potential careers.
+					</p>
+				</div>
+			</div>
+		</>
+	);
+}
 export default BasicDetailedButtons;
