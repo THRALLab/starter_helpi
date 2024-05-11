b57e9c9bc69094f931991261dbc2ac622563e011
Jasnoor Saini <jasnoor.saini@icloud.com>
2024-05-11 15:53:11 -0400

    Changed spinning loader size to better fit in Canvas

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -3,8 +3,8 @@ import { TailSpin } from "react-loader-spinner";
 const LoaderComp = () => {
     return (
         <TailSpin
-            height="100"
-            width="100"
+            height="80"
+            width="80"
             color="#4fa94d"
             ariaLabel="tail-spin-loading"
             radius="1"
