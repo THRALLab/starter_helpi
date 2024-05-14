96da5e0a648b96d1e3b8c1a8f49c8fcccd35a754
Coleman <cowalsh@udel.edu>
2024-05-13 23:36:38 -0400

    deleted repetitive inline styling

--- src/pages/BasicPage.tsx
+++ src/pages/BasicPage.tsx
@@ -273,7 +273,7 @@ const BasicPage = () => {
 		<div className="question-row">
 			<div className="question">
 				<span className="QuestionNum">#5</span> <span>
-				<Stack className="last4" gap={3} style={{marginTop: "30px"}}>
+				<Stack className="last4" gap={3}>
 				<Form.Check 
 							type="radio"
 							id="q5-Option1"
@@ -293,7 +293,7 @@ const BasicPage = () => {
 			</div>	
 			<div className="question">		
 				<span className="QuestionNum">#6</span> <span className="basic-radials">
-				<Stack  className="last4" gap={3} style={{marginTop: "30px"}}> 
+				<Stack  className="last4" gap={3}> 
 						<Form.Check 
 							type="radio"
 							id="q6-Option1"
@@ -317,7 +317,7 @@ const BasicPage = () => {
 			</div>
 			<div className="question">
 				<span className="QuestionNum">#7</span> <span>
-				<Stack className="last4" gap={3} style={{marginTop: "30px"}}> 
+				<Stack className="last4" gap={3} > 
 				<Form.Check 
 							type="radio"
 							id="q7-Option1"
@@ -337,7 +337,7 @@ const BasicPage = () => {
 			</div>
 			<div className="question">
 			<span className="QuestionNum">#8</span> <span>
-			<Stack className="last4" gap={3} style={{marginTop: "30px"}}> 
+			<Stack className="last4" gap={3}> 
 			<Form.Check 
 						type="radio"
 						id="q8-Option1"
