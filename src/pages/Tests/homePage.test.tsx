58dc46dcf74a781ec02f08bc836f315125eeaed5
Benjamin Le <benle@udel.edu>
2024-05-16 09:56:35 -0400

    added link tests

--- src/pages/Tests/homePage.test.tsx
+++ src/pages/Tests/homePage.test.tsx
@@ -1,6 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from '../../App';
 
+test('Render Home, Basic Quiz, Detailed Quiz and Results links', () => {
+    render(<App />);
+    const homeLink = screen.getByRole("link", {name:/Home/})
+    const basicLink = screen.getAllByRole("link", {name:/Basic Quiz/});
+    const detailedLink = screen.getAllByRole("link", {name:/Detailed Quiz/});
+    const resultsLink = screen.getByRole("link", {name:/Results/})
+    expect(homeLink).toBeInTheDocument()
+    expect(basicLink).toHaveLength(2);
+    expect(detailedLink).toHaveLength(2);
+    expect(resultsLink).toBeInTheDocument();
+  });
+    
 test('renders introduction text part 1', () => {
     render(<App />);
     const intro = screen.getByText(/Struggling to find your way?/i);
