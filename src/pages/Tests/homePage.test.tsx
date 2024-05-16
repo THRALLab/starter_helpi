e8b30b7da00ec1d1a6eca47facfa01eed5caf777
Benjamin Le <benle@udel.edu>
2024-05-16 12:52:12 -0400

    removed unused import and reorganized test in file

--- src/pages/Tests/homePage.test.tsx
+++ src/pages/Tests/homePage.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import App from '../../App';
-import HomePage from '../HTML/homePage';
 
 test('Render Home, Basic Quiz, Detailed Quiz and Results links', () => {
     render(<App />);
@@ -43,6 +42,16 @@ test('Renders the pie chart', () => {
   const pieChart = screen.getByTestId('pieChart');
   expect(pieChart).toBeInTheDocument();
   });
+  
+  test('Render pie chart legends', () => {
+    render(<App/>);
+    const pieLegend1 = screen.getByText(/"I Loved It!"/)
+    const pieLegend2 = screen.getByText(/"It was alright."/)
+    const pieLegend3 = screen.getByText(/"I'm not so impressed."/)
+    expect(pieLegend1).toBeInTheDocument();
+    expect(pieLegend2).toBeInTheDocument();
+    expect(pieLegend3).toBeInTheDocument();
+  })
 
 test('Render textbox for API input', () => {
   render(<App/>);
@@ -50,15 +59,7 @@ test('Render textbox for API input', () => {
   expect(textBox).toBeInTheDocument();
 })
 
-test('Render pie chart legends', () => {
-  render(<App/>);
-  const pieLegend1 = screen.getByText(/"I Loved It!"/)
-  const pieLegend2 = screen.getByText(/"It was alright."/)
-  const pieLegend3 = screen.getByText(/"I'm not so impressed."/)
-  expect(pieLegend1).toBeInTheDocument();
-  expect(pieLegend2).toBeInTheDocument();
-  expect(pieLegend3).toBeInTheDocument();
-})
+
 
 test('Render API Submit Button', () => {
   render(<App/>);
